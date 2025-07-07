4edb5bfcc6de60333a7ed955a7cf81749f6f4801
admin <133757594+eslionekim@users.noreply.github.com>
2025-07-07 21:52:31 +0900

    chapter04

--- src/ch03/test2.java
+++ src/ch03/test2.java
@@ -1,14 +1,10 @@
 package ch03;
 
-import java.util.Scanner;
-
 public class test2 {
         public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.print("100의 자리 숫자를 입력하세요.:");
-        int i = scan.nextInt();
-
-        System.out.printf("100의 자릿수는 %d, 10의 자릿수는 %d, 1의 자릿수는 %d입니다.", i / 100, (i % 100) / 10, i % 10);
-        scan.close();
+        byte num1=0;
+        byte num2=-1;
+        int result =num1&num2;
+        System.out.println(result);
     }
 }
--- src/ch04/day7_7.java
+++ src/ch04/day7_7.java
@@ -1,77 +1,10 @@
-### ch02.변수
-## variableExchange : 변수 데이터 바꾸기
-        int x=3;
-        int y=5;
+package ch04;
 
-        System.out.printf("원래는 x=%d, y=%d입니다.\n",x,y);
+import java.util.Scanner;
 
-        int box=x;
-        x=y;
-        y=box;
+public class day7_7 {
 
-        System.out.printf("바꾼값은 x=%d, y=%d 입니다",x,y);
-
-## typeChange : 다른 타입끼리 연산 
-        int i=1;
-        double d=3.56;
-        double result=i+d; //int와 double계산이니까 더 허용범위의 double
-        System.out.println("int+double="+result);
-
-        int result2=i+(int)d; //소수점 버리고 싶으면 double을 int로 강제 타입 변환
-        System.out.println("int+(int)double="+result2);
-
-        int x=2;
-        int y=3;
-        double db=x/y; //2/3=0.5이지만 int끼리 연산이니까 0이 돼. 그럼 double타입이니 0.0이겠지
-        System.out.println("x/y(int)="+db);
-
-        char c1='A';
-        char c2=1;
-        int result3=c1+c2; //둘다 int로 변환되어 계산되니 65+1=66
-        System.out.println("c1+c2="+result3);
-        System.out.println("c1+c2->char="+(char)result3);        
-
-## variableExchange : 문자열 더하기 
-        int i=1+2+8; //11
-        System.out.println("int i=1+2+8="+i); 
-
-        String s="1"+2+8; //128
-        System.out.println("String s=\"1\"+2+8="+s);
-
-        String s2="1"+"2"+8; //128
-        System.out.println("String s2=\"1\"+\"2\"+8="+s);
-
-        String s3="1"+(2+8); //110
-        System.out.println("String s3=\"1\"+(2+8)="+s3);
-
-## StringAndBasicTypeChange : 문자열과 기본 타입을 변환
-        String s="10";
-
-        int i=Integer.parseInt(s); //문자열을 int로
-        System.out.println("int i=Integer.parseInt(s)="+i);
-
-        double d=Double.parseDouble(s); //문자열을 double로
-        System.out.println("double d=Double.parseDouble(s)="+d);
-
-        System.out.println("String.valueOf(d)="+String.valueOf(d)); //double을 문자열로
-
-## introduce : 자기 소개
-        Scanner scan = new Scanner(System.in); //스캐너 스캐너이름 = 스캐너를 메모리에 넣기(시스템내부)
-        
-        System.out.print("이름을 입력하세요.:");
-        String name=scan.nextLine();
-
-        System.out.print("나이를 입력하세요.:");
-        String age=scan.nextLine();
-        int ageInt=Integer.parseInt(age);
-
-        System.out.print("학과를 입력하세요.:");
-        String major=scan.nextLine();
-
-        System.out.printf("제 이름은 "+name+" 이고, 나이는 %d살이며, 학과는 %s 입니다.",ageInt,major);
-
-### ch04
-## for, swich, while, do-while문
+    public static void main(String[] args) {
         for (int i = 1; i < 6; i++) { //한줄에 12345출력
             System.out.print(i + " ");
         }
@@ -83,7 +16,6 @@
                 System.out.print(x + " ");
             }
         }
-        
 
         System.out.println(""); //(1)+(1+2)+(1+2+3)... 10까지 내가 한 방법
         int sum = 0; //sum=sum+ i *(11-i)
@@ -142,7 +74,7 @@
         }
 
         Scanner scan = new Scanner(System.in);
-        /System.out.println("\n"); //스캔받아서 별 차례대로 찍기
+        /*System.out.println("\n"); //스캔받아서 별 차례대로 찍기
         System.out.print("별 몇개 출력:");
         int s=scan.nextInt();
         for(int i=1;i<=s;i++){ 
@@ -275,3 +207,7 @@
         }while{input!=com};
         정답 출력
       
+         */
+
+    }
+}
